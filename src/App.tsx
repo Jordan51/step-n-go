fd241390d0038136f6a7e7d62b644296e0255bc3
MORLET Jordan DGAC/CRNA-E <jordan.morlet@aviation-civile.gouv.fr>
2019-07-05 12:15:18 +0200

    FEATURE (WIP): AddTravel

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
 
+import AddTravel from "./components/AddTravel";
+
 const App: React.FC = () => {
-  return <div className="App">Hello World!</div>;
+  return (
+    <div className="App">
+      <AddTravel />
+    </div>
+  );
 };
 
 export default App;
